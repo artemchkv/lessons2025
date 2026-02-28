af8e3fbac42ad2b9febe1b124ec67ea09c791254
e-chukavov <ec@unicsoft.com>
2026-03-01 00:48:36 +0200

    first lesson varyables type

---
+++ javascript/lesson-01/index.js
@@ -0,0 +1,47 @@
+// Тип number 
+var a;
+// console.log('My variable type is:', typeof a)
+
+a = 4;
+// console.log('My variable type is:', typeof a)
+// Тип string
+var b;
+// console.log('My variable type is:', typeof b)
+console.log('d', d)
+b = 'Value zero';
+b = b.toUpperCase();
+c = b.toLowerCase();
+var d = 'hjjlkhkj';
+console.log(b, c, d)
+// Тип bool
+
+var e;
+var g = '5';
+
+e = g > a
+console.log('e type: ', e)
+
+// Тип object
+
+var array = ['d', 43535, 'jdjdj'];
+console.log(array, typeof array)
+
+console.log('first element', array[1]);
+
+var obj = {'first name': 'Yevhenii'};
+console.log('first element', obj['first name']);
+
+var obj1 = {firstName: 'Yevhenii', secondName: 'Chukavov'};
+console.log('first element', obj1.firstName);
+console.log(typeof obj1);
+
+var newOnj = {...obj1};
+obj1.secondName = 'JKHjkhjkhjklh'
+console.log('first element', newOnj);
+
+function user(name) {
+  var newName = name + ' ' + 'Chukavov';
+  return newName;
+}
+
+console.log('User:', user('Yevhenii'), typeof user);
