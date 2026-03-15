a163ebe9758803d2996f119bc9e1e2ab054e5afe
e-chukavov <ec@unicsoft.com>
2026-03-16 00:32:23 +0200

    lesson 04

---
+++ javascript/lesson-04/index.js
@@ -0,0 +1,55 @@
+// var myArray = [];
+// var second = myArray;
+
+// second[0] = '124';
+// myArray[1] = 'hjghjghhj';
+
+// console.log('vs array: ', myArray === second);
+
+// console.log('second: ', second);
+// console.log('myArray: ', myArray);
+
+// var arr = new Array(10);
+// console.log('🚀 ~ arr:', arr[3]);
+// console.log('🚀 ~ arr:', arr.length);
+
+// var arr2 = [undefined, undefined, undefined, undefined, undefined];
+// console.log('🚀 ~ arr2:', arr2);
+// console.log('🚀 ~ arr2:', arr2.length);
+
+// var arr3 = new Array('hi', 123);
+// console.log('🚀 ~ arr3:', arr3);
+// var arr4 = ['hi', 123];
+// console.log('🚀 ~ arr4:', arr4);
+
+// var arr4 = ['hi', [123, 541, 5564, 4556], { name: 'Yevhenii', age: 53 }];
+// console.log('🚀 ~ arr4:', arr4);
+// var age_key = 'age';
+// console.log('🚀 ~ arr4:', arr4[2][age_key]);
+// console.log('🚀 ~ arr4:', arr4[2].age_key);
+
+// var array4 = ['one', 'two'];
+// array4.push('three');
+// console.log('🚀 ~ array4:', array4);
+// array4.unshift('prefix');
+// console.log('🚀 ~ array4:', array4);
+// array4.pop();
+// console.log('🚀 ~ array4:', array4);
+// array4.shift();
+// console.log('🚀 ~ array4:', array4);
+
+// var str = 'шалаш';
+
+// var strArray = str.split('');
+// console.log('🚀 ~ strArray:', typeof strArray);
+
+// var newArray = strArray.reduceRight((acc, item) => acc + item, '');
+// strArray.push('-2008');
+// var strNew = strArray.join('');
+// console.log('🚀 ~ strNew:', strNew);
+// var result = str === newArray;
+// console.log('🚀 ~ result:', result);
+
+var array6 = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+var result = array6.indexOf('casmel');
+console.log('🚀 ~ result:', result);
