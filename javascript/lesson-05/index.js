7ab99465a8af1af2b1165004ced7e917b094b28a
e-chukavov <ec@unicsoft.com>
2026-03-22 00:40:02 +0200

    lesson 6

---
+++ javascript/lesson-05/index.js
@@ -0,0 +1,35 @@
+// var re1 = new RegExp('abc');
+
+// var string1 = 'ddsfsabcbde';
+
+// const result = re1.test(string1);
+// const result2 = string1.match(re1);
+// const result3 = string1.search(re1);
+// console.log('🚀 ~ result3:', result3);
+
+// console.log(result);
+// console.log('🚀 ~ result2:', result2);
+
+// var str = 'Bad Hello BAD friends bad';
+// var reg = /bad/gi;
+// var str2 = str.replace(reg, 'good');
+
+// console.log('🚀 ~ str2:', str2);
+
+// var str = '+380-99-532-77-99';
+// var reg2 = /^\+\d{3}-\d{2}-\d{3}-\d{2}-\d{2}\b/gi;
+
+// var result = reg2.test(str);
+// console.log('🚀 ~ result:', result);
+
+// If else
+
+var obj = { name: 'Yevhenii' };
+
+if (obj.name === 'Yevhenii') {
+  console.log('Name: ', obj.name);
+} else if (obj.name) {
+  console.log('Name not found, but prezent');
+} else {
+  console.log('Not enout name');
+}
