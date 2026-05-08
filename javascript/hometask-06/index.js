c207898cbf14d5eb9c1e591c33b602c19c3c32a1
Artem Chukavov <tema.chukavov@gmail.com>
2026-05-08 21:53:47 +0300

    hometask 6

---
+++ javascript/hometask-06/index.js
@@ -0,0 +1,7 @@
+var text = 'I have 1 apple, 2 oranges and 3 bananas';
+
+var result = text.replaceAll('1', 'one')
+                .replaceAll('2', 'two')
+                .replaceAll('3', 'three');
+
+console.log(result);
