67bd2d91e93d79b5b4bc90dfde4f21a260b3c949
Artem Chukavov <tema.chukavov@gmail.com>
2026-06-02 23:04:33 +0300

    tash 09 part 1

---
+++ javascript/hometask-09/index.js
@@ -0,0 +1,25 @@
+class first {
+    run() {
+        console.log('run');
+    }
+    go() {
+        console.log('go');
+    }
+    say() {
+        console.log('say');
+    }
+}
+
+class second {
+    run() {
+        console.log('second');
+    }
+    go() {
+        console.log('second');
+    }
+}
+
+class third {
+    static createobj() {
+    }
+}
