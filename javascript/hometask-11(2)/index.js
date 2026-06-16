702577f26b929ce20444712f4b1779b3e21599fe
e-chukavov <ec@unicsoft.com>
2026-06-16 22:07:23 +0300

    neew ht

--- javascript/hometask-11(2)/index.js
+++ javascript/hometask-11(2)/index.js
@@ -1,27 +1,29 @@
 class NotificationMessage {
-    static showCorrect() {
-        console.log("Password is correct");
-    }
-    static showIncorrect() {
-        console.log("Password is incorrect");
-    }
+  static showCorrect() {
+    console.log('Password is correct');
+  }
+  static showIncorrect() {
+    console.log('Password is incorrect');
+  }
 }
 
 class PasswordChecker {
-    constructor(password) {
-        this.password = "123";
+  constructor(password) {
+    this.password = '123';
+  }
+  verifyPassword(inputPassword) {
+    if (inputPassword === this.password) {
+      NotificationMessage.showCorrect();
+    } else {
+      NotificationMessage.showIncorrect();
     }
-    verifyPassword(inputPassword) {
-        if (inputPassword === this.password) {
-            NotificationMessage.showCorrect();
-        } else {
-            NotificationMessage.showIncorrect();
-        }
-    }
+  }
 }
 
-var password = prompt("Enter your password:");
-if (password !== null) {
-    const checker = new PasswordChecker();
-    checker.verifyPassword(password);
+var password = prompt('Enter your password:');
+console.log('🚀 ~ password:', password);
+var rowPAssword = password.trim();
+if (rowPAssword !== null && rowPAssword !== '') {
+  const checker = new PasswordChecker();
+  checker.verifyPassword(rowPAssword);
 }
